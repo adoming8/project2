fb52c3536ecb4af5dbd98fc36c9c9ffc8d0eb3a3
David Riveraperez <david.i.riveraperez@gmail.com>
2020-01-30 20:21:32 -0600

    re strucutred DB

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+  
+INSERT INTO Players (player_name, player_email, player_password) VALUES ('David', '@yahoo.com','Tripleta09');
+
+
+INSERT INTO Scores (player_gametagID, player_score) VALUES ('atxcrazy',75);
