cf8a20ccea420003fdbab5eed39a2685c8c7dae1
David Riveraperez <david.i.riveraperez@gmail.com>
2020-02-01 10:44:53 -0600

    Deleted DB folfer created models for player and scores

---
+++ models/score.js
@@ -0,0 +1,11 @@
+module.exports = function (sequelize, Datatypes) {
+    const Model = sequelize.define('Score', {
+        score: Datatypes.INTEGER,
+    });
+
+    Model.associate = function (models) {
+        models.Score.belongsTo(models.Player);
+    };
+
+    return Model;
+}
