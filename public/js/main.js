ef0911944dbcc417d0efad970721d42e73b54788
Cbach <ceisenbach@Codys-MacBook-Pro.local>
2020-02-04 19:51:49 -0600

    userObject array added and exported, other functionality

--- public/js/main.js
+++ public/js/main.js
@@ -175,6 +175,21 @@ clearHighscoresButton.addEventListener("click", function(event) {
     renderHighscores();
 });
 
+const loggedOutLinks = document.querySelectorAll('.logged-out');
+const loggedInLinks = document.querySelectorAll('.logged-in');
+
+const setupUI = (user) => {
+  if (user) {
+    // toggle user UI elements
+    loggedInLinks.forEach(item => item.style.display = 'block');
+    loggedOutLinks.forEach(item => item.style.display = 'none');
+  } else {
+    // toggle user elements
+    loggedInLinks.forEach(item => item.style.display = 'none');
+    loggedOutLinks.forEach(item => item.style.display = 'block');
+  }
+};
+
 // Firebase materialize components
 document.addEventListener('DOMContentLoaded', function () {
 
@@ -184,14 +199,5 @@ document.addEventListener('DOMContentLoaded', function () {
     var items = document.querySelectorAll('.collapsible');
     M.Collapsible.init(items);
 
-});
-
-// logout
-const logout = document.querySelector('#logout');
-logout.addEventListener('click', (e) => {
-    e.preventDefault();
-    auth.signOut().then(() => {
-        console.log('user signed out');
-    });
 });
 
